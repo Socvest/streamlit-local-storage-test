c1d271d80145a57b7c5f78a3e8b7aded4a9654c8
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-07 16:36:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,142 +2,105 @@ import time
 import streamlit as st
 from streamlit_local_storage import LocalStorage
 
-st.set_page_config(layout="wide")
-# if "class_init" not in st.session_state:
-#     st.session_state["class_init"] = LocalStorage()
+if "set_cookie_placement" not in st.session_state:
+    st.session_state["set_cookie_placement"] = None
 
-@st.cache_resource(experimental_allow_widgets=True)
-def LocalStore():
-    return LocalStorage()
+if "get_cookie_placement" not in st.session_state:
+    st.session_state["get_cookie_placement"] = None
+
+if "delete_cookie_placement" not in st.session_state:
+    st.session_state["delete_cookie_placement"] = None
+
+if "get_storage_list_placement" not in st.session_state:
+    st.session_state["get_storage_list_placement"] = None
+
+if "delete_storage_list_placement" not in st.session_state:
+    st.session_state["delete_storage_list_placement"] = None
+
+if "getAll_items" not in st.session_state:
+    st.session_state["getAll_items"] = False
+
+if "del_list_click" not in st.session_state:
+    st.session_state["del_list_click"] = False
+
+
+localS = LocalStorage() 
+
+cols = st.columns(3)
+
+def setCookie():
+    with st.session_state["set_cookie_placement"][0]:
+        keyItem = st.session_state["set_storage_item"] 
+        keyValue = st.session_state["set_storage_value"] 
+        localS.setItem(keyItem, keyValue)
+
+
+with cols[0].form("set_storage"):
+    st.subheader("Set Storage")
+    st.text_input("key", key="set_storage_item")
+    st.text_input("key", key="set_storage_value")
+    st.form_submit_button("set this", on_click=setCookie)
+    st.session_state["set_cookie_placement"] = st.columns(1)
+
+def get_one_item():
+    with st.session_state["get_cookie_placement"][0]:
+        get_item = localS.getItem(st.session_state["get_storage_item"])
+    st.write(get_item)
+
+with cols[1].form("get_storage", clear_on_submit=True):
+    st.subheader("Get Storage")
+    st.text_input("key", key="get_storage_item")
+    st.form_submit_button("get this", on_click=get_one_item) #, on_click=setCookie)
+    st.session_state["get_cookie_placement"] = st.columns(1)
+
+# if st.session_state["get_storage_item"] != "":
+#     get_item = localS.getItem(st.session_state["get_storage_item"])
+#     st.write(get_item)
+
+
+def deleteItem():
+    with st.session_state["delete_cookie_placement"][0]:
+        localS.deleteItem(st.session_state["delete_storage_item"])
+
+with cols[2].form("delete_item_in_storage"):
+    st.subheader("Delete Storage")
+    st.text_input("key", key="delete_storage_item")
+    st.form_submit_button("delete this", on_click=deleteItem)
+    st.session_state["delete_cookie_placement"] = st.columns(1)
+
+def getAllStorage(): 
+    st.session_state["getAll_items"] = not st.session_state["getAll_items"]
+    # with st.session_state["get_storage_list_placement"][0]:
+    #     localS.deleteAll()
     
-localS = LocalStore() #st.session_state["class_init"]
-st.write(localS.storedItems)
 
-if "get_local_storage_item" not in st.session_state:
-    st.session_state["get_local_storage_item"] = None
-if "get_storage" not in st.session_state:
-    st.session_state["get_storage"] = None
-if "test_get_click_btn" not in st.session_state:
-    st.session_state["test_get_click_btn"] = False
-if "saved_cols" not in st.session_state:
-    st.session_state["saved_cols"] = None
-
-cols = st.columns([0.5,1,1,1,0.5])
-
-def add_to_storage():
-    itemKey = st.session_state["local_storage_set_key"]
-    itemValue = st.session_state["local_storage_set_value"]
-    with st.session_state["saved_cols"][0]:
-        localS.setItem(itemKey, itemValue)
-  
-
-cols[1].subheader("add to local storage")
-with cols[1].form("add_local_storage"):
-    # add_cols = st.columns(2)
-    st.text_input("key", key="local_storage_set_key")
-    st.text_input("value", key="local_storage_set_value")
-    st.form_submit_button("submit", on_click=add_to_storage)
-    st.session_state["saved_cols"] = st.columns(1)
+with cols[1].form("get_all_storage"):
+    st.subheader("Get all of storage")
+   # st.multiselect("keys", options=["One", "two", "three", "four", "five", "six", "seven"], key="get_list_of_storage")
+    st.form_submit_button("Get all storage", on_click=getAllStorage)
+    st.session_state["get_storage_list_placement"] = st.columns(1)
 
 
-cols[2].subheader("get from local storage")
-with cols[2].form("get_data"):
-    st.text_input("key", key="get_local_storage_v")
-    st.form_submit_button("Submit") #, on_click=testFunc) 
+if st.session_state["getAll_items"]:
+    all_storage_ = localS.getAll()
+    st.write(all_storage_)
 
-if st.session_state["get_local_storage_v"] != "": 
-    val_ = localS.getItem(st.session_state["get_local_storage_v"])
-    st.session_state["get_storage"] = val_
+
+def deleteFromStorage(): 
+    st.session_state["del_list_click"] = not st.session_state["del_list_click"]
+    # with st.session_state["get_storage_list_placement"][0]:
+    #     localS.deleteAll()
     
-    cols[2].write(st.session_state["get_storage"])
 
-def delete_from_storage():
-    itemKey = st.session_state["local_storage_delete_key"]
-    localS.deleteItem(itemKey) 
-
-cols[3].subheader("delete to local storage")
-with cols[3].form("delete_local_storage"):
-    st.text_input("key", key="local_storage_delete_key")
-    st.form_submit_button("submit") #, on_click=delete_from_storage)
-
-if st.session_state["local_storage_delete_key"] != "":
-    itemKey_ = st.session_state["local_storage_delete_key"]
-    localS.deleteItem(itemKey_) 
-    # LocalStore.clear()
+with cols[2].form("delete_all_storage"):
+    st.subheader("Delete all of storage")
+   # st.multiselect("keys", options=["One", "two", "three", "four", "five", "six", "seven"], key="get_list_of_storage")
+    st.form_submit_button("Delete all storage", on_click=deleteFromStorage)
+    st.session_state["delete_storage_list_placement"] = st.columns(1)
 
 
-# st.write(st.session_state["get_storage"])
-
-
-
-
-
-# st.set_page_config(layout="wide")
-
-# localS = LocalStorage()
-
-
-# if "start" not in st.session_state:
-#     st.session_state["start"] = 0
-
-# if "get_local_storage_item" not in st.session_state:
-#     st.session_state["get_local_storage_item"] = None
-# if "get_storage" not in st.session_state:
-#     st.session_state["get_storage"] = None
-# if "test_get_click_btn" not in st.session_state:
-#     st.session_state["test_get_click_btn"] = False
-# if "saved_cols" not in st.session_state:
-#     st.session_state["saved_cols"] = None
-
-# cols = st.columns([0.5,1,1,1,0.5])
-
-# def add_to_storage():
-#     itemKey = st.session_state["local_storage_set_key"]
-#     itemValue = st.session_state["local_storage_set_value"]
-#     with st.session_state["saved_cols"][0]:
-#         localS.setItem(itemKey, itemValue)
-  
-
-# cols[1].subheader("add to local storage")
-# with cols[1].form("add_local_storage"):
-#     # add_cols = st.columns(2)
-#     st.text_input("key", key="local_storage_set_key")
-#     st.text_input("value", key="local_storage_set_value")
-#     st.form_submit_button("submit", on_click=add_to_storage)
-#     st.session_state["saved_cols"] = st.columns(1)
-
-# def testFunc():
-#     st.session_state["test_get_click_btn"] = True
-
-# cols[2].subheader("get from local storage")
-# with cols[2].form("get_data"):
-#     st.text_input("key", key="get_local_storage_v")
-#     st.form_submit_button("Submit") #, on_click=testFunc) 
-
-# if st.session_state["get_local_storage_v"] != "": # and st.session_state["test_get_click_btn"] == True:
-#     if st.session_state["get_storage"] != None and st.session_state["get_storage"]["data"] == "nothing":
-#         st.session_state["get_storage"] = {"item": st.session_state["get_local_storage_v"], "data": "nothing"}
-#     else:
-#         val_ = localS.getItem(st.session_state["get_local_storage_v"], key="test_get_item")
-#         st.session_state["get_storage"] = st.session_state["test_get_item"]
-#         st.session_state["start"] += 1 
-#         st.write(st.session_state["start"])
+if st.session_state["del_list_click"]:
     
-#     cols[2].write(st.session_state["get_storage"])
-
-# def delete_from_storage():
-#     itemKey = st.session_state["local_storage_delete_key"]
-#     localS.deleteItem(itemKey) 
-
-# cols[3].subheader("delete to local storage")
-# with cols[3].form("delete_local_storage"):
-#     st.text_input("key", key="local_storage_delete_key")
-#     st.form_submit_button("submit") #, on_click=delete_from_storage)
-
-# if st.session_state["local_storage_delete_key"] != "":
-#     # st.session_state["test_get_click_btn"] = True
-#     itemKey_ = st.session_state["local_storage_delete_key"]
-#     localS.deleteItem(itemKey_) 
-#     st.session_state["get_storage"] = {"item":itemKey_, "data":"nothing"}
+    localS.deleteAll()
 
 
